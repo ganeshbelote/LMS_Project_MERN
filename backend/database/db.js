c55030bc758d97ba5d4f3b6102b62a0aa319b6c4
ganeshbelote <ganeshbelote18@gmail.com>
2025-07-27 21:04:33 +0530

    Fixed : git add problem related to documentations

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,11 +1,19 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
-export const connectDB = async (req,res) => {
-    try {
-        mongoose.connect(process.env.MONGO_URL)
-        console.log("Database Connected 👍")
-    } catch (error) {
-        console.log("Database connection Failed !!",error.message)
-        process.exit(1)
-    }
+export const connectDB = async () => {
+  try {
+    mongoose.connect(process.env.MONGO_URL, {
+      dbName: 'LMS',
+      autoIndex: true,
+      maxPoolSize: 10,
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000,
+      retryWrites: true,
+      w: 'majority'
+    })
+    console.log('Database Connected 👍')
+  } catch (error) {
+    console.log('Database connection Failed !!', error.message)
+    process.exit(1)
+  }
 }
